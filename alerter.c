ded800022590a2a6fb81af13cfd8f796650da9cc
SudheerPedagadi <pedagadivenkat083@gmail.com>
2024-08-19 12:49:01 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -6,7 +6,7 @@ int alertFailureCount = 0;
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok, 500 for not-ok based on temperature
-    if (celcius > 200) {
+    if (celcius > 250) {
         return 500;
     }
     return 200;
@@ -16,15 +16,27 @@ void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
-        // Increment the failure count
-        alertFailureCount++;
+        alertFailureCount+=0;
     }
 }
 
+void failureCount() {
+// Reseting the alertFailureCount to 0 
+    alertFailureCount = 0;
+    alertInCelcius(200); 
+    printf("AlertFailureCount = %d\n", alertFailureCount);
+    assert(alertFailureCount == 0);  
+    alertInCelcius(300); 
+    printf("AlertFailureCount = %d\n", alertFailureCount);
+    assert(alertFailureCount == 1); 
+    alertInCelcius(150);   
+    printf("AlertFailureCount = %d\n", alertFailureCount);
+    assert(alertFailureCount == 1);  
+    printf("All tests passed successfully!\n");
+}
+
 int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(401.6);
-    assert(alertFailureCount == 2); // We expect two failures
+    failureCount();
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;

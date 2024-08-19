1dae516b51c0ce65427093791815ddaf49f36701
SudheerPedagadi <pedagadivenkat083@gmail.com>
2024-08-19 12:54:09 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -5,7 +5,7 @@ char size(int cms) {
     char sizeName = '\0';
     if(cms < 38) {
         sizeName = 'S';
-    } else if(cms >= 38 && cms <= 42) { // Corrected the boundary condition
+    } else if(cms > 38 && cms < 42) { // Corrected the boundary condition
         sizeName = 'M';
     } else if(cms > 42) {
         sizeName = 'L';
@@ -18,9 +18,11 @@ int main() {
     assert(size(40) == 'M');
     assert(size(43) == 'L');
     // Introduce a failing test case
-    assert(size(10) == 'M'); // This should fail as 10 is 'S'
-    assert(size(38) == 'L'); // This should fail as 38 is 'M'
-    assert(size(45) == 'S'); // This should fail as 38 is 'L'
+    assert(size(42) == 'M');
+    assert(size(38) == '\0');
+    assert(size(38) == 'M');
+    assert(size(38) == 'S');
+    assert(size(42) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
 }
